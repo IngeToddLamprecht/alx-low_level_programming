db82acc24266c617161450a2b2135bcaa5954fe6
Inge Lamprecht <ingetoddlamp@gmail.com>
2022-10-20 05:05:43 -0700

    task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,16 +3,25 @@
 /**
  * print_numbers - Print the numbers 0 to 9
  *
- * Return: The numbers 0 to 9
+ * Description: Prints numbers excluding 2 and 4
+ *
+ * Return: Numbers 0 to 9
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	int x;
+	int x = 0;
 
-	for (x = 0; x <= 9; x++)
+	for (; x <= 9; x++)
 	{
-		_putchar(x + '0');
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
 	}
 
-	_putchar('\n');
+
 }
