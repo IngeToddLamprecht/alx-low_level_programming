e8fc1c925d56c44532a2576d78baf7ad9b88b8eb
Inge Lamprecht <ingetoddlamp@gmail.com>
2022-10-27 06:40:06 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters to uppercase letters
+ * @p: String
+ *
+ * Return: char var
+ */
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+
+		a++;
+	}
+
+	return (p);
+}
