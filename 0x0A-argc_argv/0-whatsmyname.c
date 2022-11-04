c27f581357f46ae5ab42539b4e1bdad22e38b423
Inge Lamprecht <ingetoddlamp@gmail.com>
2022-11-03 22:06:26 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints name
+ * @argc: Command lines
+ * @argv: Array
+ *
+ * Return: empty
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
